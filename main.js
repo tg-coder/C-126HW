43e7323a740d0e49805cc726db6ac90972189218
tg-coder <76582848+tg-coder@users.noreply.github.com>
2021-10-02 08:13:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-score = 0;
+scoreLeftWrist = 0;
 left_wrist_X = 0;
 left_wrist_Y = 0;
 right_wrist_X = 0;
 right_wrist_Y = 0;
 song_1 = "";
 song_2 = "";
+scoreRightWrist = 0;
+song_status = "";
 function preload()
 {
     song = loadSound("music.mp3");
@@ -36,22 +38,13 @@ function draw()
     document.getElementById("volume").innerHTML = "Volume = "+volume;
     song.setVolume(volume);
     stroke("#008080");
-    if(scoreLeftWrist>0.2)
-    {
-    circle(left_wrist_x, left_wrist_y, 20);
-    InNumberleftwristy = Number(left_wrist_y);
-    remove_decimals = floor(InNumberleftwristy);
-    volume = remove_decimals/500;
-    document.getElementById("volume").innerHTML = "Volume = "+volume;
-    song.setVolume(volume);
-    }
-
 }
 function gotPoses(results)
 {
     if(results.length>0)
     {
         console.log(results);
+        scoreLeftWrist = results[0];
         left_wrist_X = results[0].pose.leftWrist.X;
         left_wrist_Y = results[0].pose.leftWrist.Y;
         console.log("Left wrist x = " + left_wrist_X +"Left wrist y = " +left_wrist_Y);
